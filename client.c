ea334469e5ac6689f7a9e65219a68d52ef374956
Mikrokosmos <117502741+sarah20037@users.noreply.github.com>
2025-04-09 09:34:16 +0530

    Add files via upload

--- client.c
+++ client.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<arpa/inet.h>
 
-int main() {
+int main()
+{
     int sockfd;
     struct sockaddr_in server;
-    char buffer[1024] = "Hello Server";
+    char buffer[1024];
+    char filename[1024];
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    server.sin_family=AF_INET;
+    server.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server.sin_port=htons(8087);
 
-    connect(sockfd, (struct sockaddr *)&server, sizeof(server));
-    send(sockfd, buffer, strlen(buffer), 0);
-    read(sockfd, buffer, sizeof(buffer));
-    printf("Response: %s\n", buffer);
+    connect(sockfd,(struct sockaddr*)&server,sizeof(server));
+
+    printf("Enter a filename");
+    scanf("%s",filename);
+
+    send(sockfd,filename,strlen(filename),0);
+    
+    while(recv(sockfd,buffer,sizeof(buffer),0)>0)
+    {
+        printf("%s",buffer);
+        memset(buffer,0,sizeof(buffer));
+
+    }
     close(sockfd);
-
     return 0;
 }
--- server.c
+++ server.c
@@ -1,32 +1,68 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-void main()
+int main()
 {
-    int sockfd,newsock;
-    struct sockaddr_in server,client;
-    socklen_t len = sizeof(client);
+    int sockfd, newsock;
     char buffer[1024];
-    
-    sockfd=(AF_INET,SOCK_STREAM,0);
-    server.sin_family=AF_INET;
-    server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port=htons(8080);
-    
-    bind(sockfd,struct(sockaddr)&server,sizeof(server));
-    
-    listen(sockfd.5);
-    printf("Waiting for message...\n");
-    
-    newsock=accept(sockfd,struct(sockaddr)&client,sizeof(client));
-    read(newsock,buffer,sizeof(buffer));
-    printf("%s",buffer);
-    send(newsock,"Hello from server",17,0);
-    
+    char filename[1024];
+    struct sockaddr_in server, client;
+    socklen_t len = sizeof(client);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8087);
+
+    if (bind(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        perror("Bind failed");
+        close(sockfd);
+        exit(1);
+    }
+
+    listen(sockfd, 5);
+    printf("Waiting for connection...\n");
+
+    newsock = accept(sockfd, (struct sockaddr*)&client, &len);
+    if (newsock < 0) {
+        perror("Accept failed");
+        close(sockfd);
+        exit(1);
+    }
+
+    int recv_len = recv(newsock, filename, sizeof(filename) - 1, 0);
+    if (recv_len < 0) {
+        perror("Receive failed");
+        close(newsock);
+        close(sockfd);
+        exit(1);
+    }
+
+    filename[recv_len] = '\0';  // Null-terminate filename
+    printf("Received filename: %s\n", filename);
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("File open error");
+        send(newsock, "File Not Found", strlen("File Not Found"), 0);
+    } else {
+        while (fgets(buffer, sizeof(buffer), file) != NULL) {
+            send(newsock, buffer, strlen(buffer), 0);
+            usleep(10000);  // slight delay to avoid buffer mix
+        }
+        fclose(file);
+    }
+
     close(newsock);
     close(sockfd);
-    
+
+    return 0;
 }
